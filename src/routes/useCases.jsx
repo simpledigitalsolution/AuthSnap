9713c3f09eb3961460e4b358d2aabd8fd510e526
Max Faust <tcthinker@gmail.com>
2023-11-01 10:43:59 -0500

    useCases update and styling

--- src/routes/useCases.jsx
+++ src/routes/useCases.jsx
@@ -5,7 +5,7 @@ export default function UseCases() {
   const [selectedItems, setSelectedItems] = useState([]);
   const [selectedSubtexts, setSelectedSubtexts] = useState([]);
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [showInitialList, setShowInitialList] = useState(true); // new state variable
+  const [showInitialList, setShowInitialList] = useState(true);
 
   const availableItems = [
     {
@@ -127,74 +127,84 @@ export default function UseCases() {
   }
 
   return (
-    <div className="p-8">
-      {showInitialList && ( // conditionally render the initial item list
-        <ul role="list" className="divide-y divide-gray-100">
-          {availableItems.map((item, index) => (
-            <li
-              key={index}
-              className="flex items-center justify-between gap-x-6 py-5"
-            >
-              <div className="flex min-w-0 gap-x-4">
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    {item.text}
-                  </p>
-                </div>
-              </div>
-              <button
-                onClick={() => {
-                  console.log(selectedItems);
-                  handleItemSelection(item);
-                }}
-                className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    <>
+      <div className="p-8">
+        {showInitialList && ( // conditionally render the initial item list
+          <ul role="list" className="divide-y divide-gray-100">
+            {availableItems.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between gap-x-6 py-5"
               >
-                Select
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-      {!showInitialList && ( // conditionally render the selected item subtext list
-        <div>
-          <h1 className="text-2xl font-bold">Selected Item:</h1>
-          <ul className="divide-y divide-gray-100">
-            {selectedItems.map((item, index) => (
-              <li key={index} className="mt-2">
-                <h1 className="text-sm font-semibold underline leading-6 text-gray-900">
-                  {item.text}
-                </h1>
-                <ul className="list-disc pl-8">
-                  {item.subtexts.map((subtext, index) => (
-                    <li key={index} className="mt-2">
-                    <button
-                        onClick={() => {
-                            console.log(selectedSubtexts);
-                            handleSubtextSelection(subtext);
-                        }}
-                        className={`rounded-full px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
-                            selectedSubtexts.includes(subtext) ? 'bg-green-200' : 'bg-white '
-                        }`}
-                    >
-                        {subtext}
-                    </button>
-                    </li>
-                  ))}
-                </ul>
+                <div className="flex min-w-0 gap-x-4">
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                      {item.text}
+                    </p>
+                  </div>
+                </div>
+                <button
+                  onClick={() => {
+                    console.log(selectedItems);
+                    handleItemSelection(item);
+                  }}
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Select
+                </button>
               </li>
             ))}
           </ul>
-          <button
-            onClick={() => {
-              console.log(selectedSubtexts);
-              handleSubmit();
-            }}
-            className={`flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0`}
-          >
-            View Selected
-          </button>
-        </div>
-      )}
-    </div>
+        )}
+        {!showInitialList && ( // conditionally render the selected item subtext list
+          <div className="flex flex-col">
+            <ul className="divide-y divide-gray-100">
+              {selectedItems.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex items-center justify-between gap-x-6 py-5"
+                >
+                  <ul className="list-disc pl-8">
+                    {item.subtexts.map((subtext, index) => (
+                      <li key={index} className="mt-2">
+                        <div className="flex min-w-0 gap-x-4">
+                          <div className="min-w-0 flex-auto">
+                            <p className="text-sm font-semibold leading-6 text-gray-900">
+                              {subtext}
+                            </p>
+                          </div>
+                        </div>
+                        <button
+                          onClick={() => {
+                            console.log(selectedSubtexts);
+                            handleSubtextSelection(subtext);
+                          }}
+                          className={`rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
+                            selectedSubtexts.includes(subtext)
+                              ? "bg-green-600"
+                              : "bg-indigo-600"
+                          }`}
+                        >
+                          Select
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={() => {
+                console.log(selectedSubtexts);
+                handleSubmit();
+              }}
+              className={`flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0`}
+            >
+              View Selected
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }

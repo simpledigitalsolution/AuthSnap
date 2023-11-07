836bb3ec44ef075a78c97ef03fb47a21beb5444c
Max Faust <tcthinker@gmail.com>
2023-11-06 21:18:16 -0600

    back button navigates through lists

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Login from "./components/login";
-import UseCases from "./components/useCases";
+import UseCasesList from "./components/useCasesList";
 
 export default function App() {
+
   return (
     <div className="h-full min-h-full">
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index path="login" element={<Login />} />
-          <Route path="useCases" element={<UseCases />} />
+       <Route path="/useCasesList" element={<UseCasesList />} />
         </Route>
       </Routes>
     </div>
--- src/components/useCasesList.jsx
+++ src/components/useCasesList.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import BackButton from "./backButton";
 
-// TODO: refactor subtexts into its own component receiving props from UseCases
-export default function UseCases() {
+export default function UseCasesList() {
   const [items, setItems] = useState([]);
   const [selectedItems, setSelectedItems] = useState([]);
   const [selectedSubtexts, setSelectedSubtexts] = useState([]);
@@ -65,18 +63,39 @@ export default function UseCases() {
     setIsSubmitted(true);
   };
 
+  const handleBack = () => {
+    setSelectedItems([]);
+    setShowInitialList(true);
+  };
+
   if (isSubmitted) {
     return (
-      <div className="p-4">
-        <BackButton />
+      <div className="p-8">
+        <button
+          onClick={() => {
+            setIsSubmitted(false);
+          }}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Back
+        </button>
         <h1 className="text-2xl font-bold">Selected Subtexts:</h1>
-        <ul className="list-disc pl-8">
-          {items.map((item, index) => (
-            <li key={index} className="mt-2">
-              {item}
-            </li>
-          ))}
-        </ul>
+        <ul role="list" className="divide-y divide-gray-100">
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between gap-x-6 py-5"
+              >
+                <div className="flex min-w-0 gap-x-4">
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                      {item}
+                    </p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
       </div>
     );
   }
@@ -84,7 +103,6 @@ export default function UseCases() {
   return (
     <>
       <div className="p-8">
-        <BackButton />
         {showInitialList && ( // conditionally render the initial item list
           <ul role="list" className="divide-y divide-gray-100">
             {availableItems.map((item, index) => (
@@ -101,7 +119,6 @@ export default function UseCases() {
                 </div>
                 <button
                   onClick={() => {
-                    console.log(selectedItems);
                     handleItemSelection(item);
                   }}
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -113,10 +130,18 @@ export default function UseCases() {
           </ul>
         )}
         {!showInitialList && ( // conditionally render the selected item subtext list
-          <div className="flex flex-col">
+          <div >
+            <button
+              onClick={() => {
+                handleBack();
+              }}
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Back
+            </button>
             <ul role="list" className="flex flex-col divide-y divide-gray-100">
               {selectedItems.map((item, index) => (
-                <h1 className="flex flex-col gap-y-4 p-4" key={index}>
+                <h1 className="flex flex-col gap-y-4 " key={index}>
                   <h1 className="text-2xl font-bold underline">{item.text}</h1>
                   <ul className="flex flex-col list-disc divide-y divide-gray-100">
                     {item.subtexts.map((subtext, index) => (
@@ -133,7 +158,6 @@ export default function UseCases() {
                         </div>
                         <button
                           onClick={() => {
-                            console.log(selectedSubtexts);
                             handleSubtextSelection(subtext);
                           }}
                           className={`rounded-md  px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
@@ -152,7 +176,6 @@ export default function UseCases() {
             </ul>
             <button
               onClick={() => {
-                console.log(selectedSubtexts);
                 handleSubmit();
               }}
               className={`flex w-full items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:outline-offset-0`}
